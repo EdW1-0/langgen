7a64c61ae47331f8d39e018729cb40e6374ab481
EdW1-0 <edmundwarrick@hotmail.co.uk>
2024-07-26 02:06:30 +0100

    Very basic database access

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/char")
+def char():
+    conn = sqlite3.connect('IPA.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM PHONEMES")
+    retstr = str(c.fetchall())
+    conn.close()
+    return retstr
